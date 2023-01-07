23caedce43316582663c5991246d5a2f05374137
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2023-01-07 20:27:25 +0800

    Physics-based movement

--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
-
 # movement variable
 @export var mMoveSpeed : float = 100
 @onready var mRay = $RayCast2D
+
 var mTileSize = 16
 var mInputs = {
 	"left": Vector2.LEFT,
@@ -11,68 +11,32 @@ var mInputs = {
 	"up": Vector2.UP,
 	"down": Vector2.DOWN,
 }
-var mMoving = false
-var mTilePerSec = 0
 
-# queue movement (so player can pre-enter movement before reaching end)
-@export var mQueueInputDuration : float = 0.24
-@onready var mQueueInputTimer = $Timer
-var mQueueInput
+var mMoving = Vector2.ZERO
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# calculate move duration per tile
-	mTilePerSec = mTileSize / mMoveSpeed
+	_snap_to_tile()
 
-	# reset position to tile
+
+func _physics_process(delta):
+	var collision = move_and_collide(mMoving * delta * mMoveSpeed)
+	if collision:
+		mMoving = Vector2.ZERO
+		_snap_to_tile()
+	
+	if mMoving == Vector2.ZERO:
+		for dir in mInputs:
+			if Input.is_action_pressed(dir):
+				mMoving = mInputs[dir]
+				var targetPos = mMoving * mTileSize
+				mRay.target_position = targetPos
+				mRay.force_raycast_update()
+				
+				
+# reset position to tile
+func _snap_to_tile():
 	position -= Vector2.ONE * mTileSize * 0.5
 	position = position.snapped(Vector2.ONE * mTileSize)
 	position += Vector2.ONE * mTileSize * 0.5
-
-	# setup debounce timer
-	mQueueInputTimer.set_wait_time(mQueueInputDuration)
-	mQueueInputTimer.set_one_shot(true)
-	mQueueInputTimer.connect("timeout", func (): 
-		mQueueInput = null
-	)
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-func _unhandled_input(event):
-	for dir in mInputs.keys():
-		if event.is_action_pressed(dir):
-			if (mMoving):
-				queueInput(dir)
-			else:
-				move(dir)
-	pass
-
-func move(dir):
-	var targetPos = mInputs[dir] * mTileSize
-	mRay.target_position = targetPos
-	mRay.force_raycast_update()
-	if !mRay.is_colliding():
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", targetPos, mTilePerSec).set_trans(Tween.TRANS_LINEAR).as_relative()
-		tween.connect("finished", func (): 
-			move(dir)
-		)
-		mMoving = true
-	elif mQueueInput != null:
-		move(mQueueInput)
-		mQueueInput = null
-	else:
-		mMoving = false
-	pass
-
-func queueInput(dir):
-	# stop timer and restart it
-	if !mQueueInputTimer.is_stopped():
-		mQueueInputTimer.stop()
-	# record to mQueueInput
-	mQueueInput = dir
-	mQueueInputTimer.start()
-	pass
