35e2639c94997cb738ec452dc4d058bab6bf83fa
Batbro051939 <isaacleepountney@gmail.com>
2023-01-08 19:42:22 +0800

    Level Manager changes

--- collectible/collectible.gd
+++ collectible/collectible.gd
@@ -3,35 +3,35 @@ extends Area2D
 
 
 var mTileTypeToFrame = {
-    "collectible": 0,
-    "trap_pothole": 3,
-    "trap_outside": 4,
+	"collectible": 0,
+	"trap_pothole": 3,
+	"trap_outside": 4,
 }
 
 var mIsTrap : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    connect("body_entered", _on_body_entered)
-    pass # Replace with function body.
+	connect("body_entered", _on_body_entered)
+	pass # Replace with function body.
 
 func _on_body_entered(body):
-    if body.get_script() == Player:
-        if !mIsTrap:
-            # TODO: add points
-            body.stop_at(position)
-            get_parent().collect(self)
-            queue_free()
-        else:
-            body.stop_at(position)
-            body.die()
+	if body.get_script() == Player:
+		if !mIsTrap:
+			# TODO: add points
+			body.stop_at(position)
+			get_parent().collect(self)
+			queue_free()
+		else:
+			body.stop_at(position)
+			body.die()
 
 
 func setType(tileId):
-    if tileId.begins_with("collectible"):
-        $AnimatedSprite2D.set_frame(mTileTypeToFrame["collectible"])
-        mIsTrap = false
-    elif tileId.begins_with("trap"):
-        $AnimatedSprite2D.set_frame(mTileTypeToFrame[tileId])
-        mIsTrap = true
-    pass
+	if tileId.begins_with("collectible"):
+		$AnimatedSprite2D.set_frame(mTileTypeToFrame["collectible"])
+		mIsTrap = false
+	elif tileId.begins_with("trap"):
+		$AnimatedSprite2D.set_frame(mTileTypeToFrame[tileId])
+		mIsTrap = true
+	pass
--- managers/CollectibleManager.gd
+++ managers/CollectibleManager.gd
@@ -6,32 +6,32 @@ extends Node
 var mCollectibleList: Array[Collectible] = []
 
 func cleanUp() -> void:
-    for n in get_children():
-        remove_child(n)
-        n.queue_free()
+	for n in get_children():
+		remove_child(n)
+		n.queue_free()
 
 func spawnCollectibles():
-    cleanUp()
-    var tileMap = get_node("/root/Game/LevelTemplate/TileMap")
-    if tileMap:
-        var layerCount = tileMap.get_layers_count()
-        for layerIndex in layerCount:
-            var usedCells = tileMap.get_used_cells(layerIndex)
-            for cellCoord in usedCells:
-                var tileData = tileMap.get_cell_tile_data(layerIndex, cellCoord)
-                var tileId = tileData.get_custom_data("id")
-                if tileId.begins_with("collectible") or tileId.begins_with("trap"):
-                    var node = mCollectiblePackedScene.instantiate()
-                    node.setType(tileId)
-                    node.set_position(tileMap.map_to_local(cellCoord))
-                    add_child.call_deferred(node)
-                    tileMap.erase_cell(layerIndex, cellCoord)
-                    if !node.mIsTrap:
-                        mCollectibleList.append(node)
-    pass
+	cleanUp()
+	var tileMap = get_node("/root/Game/LevelTemplate/TileMap")
+	if tileMap:
+		var layerCount = tileMap.get_layers_count()
+		for layerIndex in layerCount:
+			var usedCells = tileMap.get_used_cells(layerIndex)
+			for cellCoord in usedCells:
+				var tileData = tileMap.get_cell_tile_data(layerIndex, cellCoord)
+				var tileId = tileData.get_custom_data("id")
+				if tileId.begins_with("collectible") or tileId.begins_with("trap"):
+					var node = mCollectiblePackedScene.instantiate()
+					node.setType(tileId)
+					node.set_position(tileMap.map_to_local(cellCoord))
+					add_child.call_deferred(node)
+					tileMap.erase_cell(layerIndex, cellCoord)
+					if !node.mIsTrap:
+						mCollectibleList.append(node)
+	pass
 
 
 func collect(_collectible):
-    mCollectibleList.erase(_collectible)
-    if mCollectibleList.size() == 0:
-        get_node("/root/LevelManager").nextLevel()
+	mCollectibleList.erase(_collectible)
+	if mCollectibleList.size() == 0:
+		get_node("/root/LevelManager").nextLevel()
