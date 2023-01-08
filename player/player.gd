dfc23480658ce17c0f364dd1af0ecba7dfdc4e1b
shaun'yeap <shaunho9057@gmail.com>
2023-01-08 15:59:44 +0800

    - Apply player tilemap to player - Added player animation

--- player/player.gd
+++ player/player.gd
@@ -13,13 +13,28 @@ var mInputs: Dictionary = {
     "down": Vector2.DOWN,
 }
 
-var mMoving: Vector2 = Vector2.ZERO
+var mDirection: Vector2 = Vector2.ZERO
+var mMoving: bool = false
 var mTarget
 
 
 # Make player stop at target position
 func stop_at(target) -> void:
     mTarget = target
+
+
+func _move_toward(dir) -> void:
+    mMoving = true
+    mDirection = mInputs[dir]
+    $AnimationTree.set("parameters/idle/blend_position", mDirection)
+    $AnimationTree.set("parameters/walk/blend_position", mDirection)
+    $AnimationTree.get("parameters/playback").travel("walk")
+
+
+func stop_moving() -> void:
+    mMoving = false
+    _snap_to_tile()
+    $AnimationTree.get("parameters/playback").travel("idle")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -29,26 +44,26 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-    if mMoving == Vector2.ZERO:
+    if not mMoving:
         for dir in mInputs:
             if Input.is_action_pressed(dir):
-                mMoving = mInputs[dir]
+                _move_toward(dir)
                 _update_raycast()
 
     if mTarget != null and position.distance_squared_to(mTarget) < pow(delta * mMoveSpeed, 2):
         position = mTarget
         mTarget = null
-        mMoving = Vector2.ZERO
-        _snap_to_tile()
+        stop_moving()
+        _bouncing_effect()
 
-    var collision: KinematicCollision2D = move_and_collide(mMoving * delta * mMoveSpeed)
-    if collision:
-        _on_collision(collision)
+    if mMoving:
+        var collision: KinematicCollision2D = move_and_collide(mDirection * delta * mMoveSpeed)
+        if collision:
+            _on_collision(collision)
 
 
 func _on_collision(collision: KinematicCollision2D) -> void:
-    mMoving = Vector2.ZERO
-    _snap_to_tile()
+    stop_moving()
 
     var collided_tile_map: TileMap = collision.get_collider() as TileMap
 
@@ -58,6 +73,8 @@ func _on_collision(collision: KinematicCollision2D) -> void:
     var layers_with_walls := [0, 1]
     for layer in layers_with_walls:
         var source_id := collided_tile_map.get_cell_source_id(layer, tile_coords, false)
+        if source_id == -1:
+            continue
         var source: TileSetAtlasSource = collided_tile_map.tile_set.get_source(source_id)
         if source:
             var atlas_coords := collided_tile_map.get_cell_atlas_coords(layer, tile_coords, false)
@@ -65,10 +82,7 @@ func _on_collision(collision: KinematicCollision2D) -> void:
 
             var is_wall = tile_data.get_custom_data("is_wall")
             if is_wall:
-                var tween = get_tree().create_tween()
-                tween.tween_property($AnimatedSprite2D, "scale", Vector2(1.1, 1.1), 0.1).set_trans(Tween.TRANS_ELASTIC)
-                tween.tween_property($AnimatedSprite2D, "scale", Vector2.ONE, 0.1).from_current().set_trans(Tween.TRANS_BOUNCE)
-
+                _bouncing_effect()
 
 # Reset position to tile
 func _snap_to_tile() -> void:
@@ -79,6 +93,12 @@ func _snap_to_tile() -> void:
 
 # Reset raycast
 func _update_raycast() -> void:
-    var targetPos = mMoving * mTileSize
+    var targetPos = mDirection * mTileSize
     mRay.target_position = targetPos
     mRay.force_raycast_update()
+
+
+func _bouncing_effect() -> void:
+    var tween = get_tree().create_tween()
+    tween.tween_property($AnimatedSprite2D, "scale", Vector2(1.1, 1.1), 0.1).set_trans(Tween.TRANS_ELASTIC)
+    tween.tween_property($AnimatedSprite2D, "scale", Vector2.ONE, 0.1).from_current().set_trans(Tween.TRANS_BOUNCE)
