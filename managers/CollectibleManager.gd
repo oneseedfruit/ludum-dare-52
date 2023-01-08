33344e5c810a01e9849e198f68ee2e26f34928c9
Batbro051939 <isaacleepountney@gmail.com>
2023-01-08 14:57:51 +0800

    Fixed all levels
    Some had minor problems with the puzzle but are now resolved

--- managers/CollectibleManager.gd
+++ managers/CollectibleManager.gd
@@ -7,23 +7,23 @@ extends Node
 var mCollectibleList: Array[Collectible] = []
 
 func spawnCollectibles():
-    var tileMap = get_node("/root/Game/LevelTemplate/TileMap")
-    if tileMap:
-        var usedCells = tileMap.get_used_cells(mTileMapObjectLayer)
-        for cellCoord in usedCells:
-            var tileData = tileMap.get_cell_tile_data(mTileMapObjectLayer, cellCoord)
-            var tileId = tileData.get_custom_data("id")
-            if tileId.begins_with("collectible"):
-                var node = mCollectiblePackedScene.instantiate()
-                node.setType(tileId)
-                node.set_position(tileMap.map_to_local(cellCoord))
-                add_child.call_deferred(node)
-                tileMap.erase_cell(mTileMapObjectLayer, cellCoord)
-                mCollectibleList.append(node)
-    pass
+	var tileMap = get_node("/root/Game/LevelTemplate/TileMap")
+	if tileMap:
+		var usedCells = tileMap.get_used_cells(mTileMapObjectLayer)
+		for cellCoord in usedCells:
+			var tileData = tileMap.get_cell_tile_data(mTileMapObjectLayer, cellCoord)
+			var tileId = tileData.get_custom_data("id")
+			if tileId.begins_with("collectible"):
+				var node = mCollectiblePackedScene.instantiate()
+				node.setType(tileId)
+				node.set_position(tileMap.map_to_local(cellCoord))
+				add_child.call_deferred(node)
+				tileMap.erase_cell(mTileMapObjectLayer, cellCoord)
+				mCollectibleList.append(node)
+	pass
 
 
 func collect(_collectible):
-    mCollectibleList.erase(_collectible)
-    if mCollectibleList.size() == 0:
-        get_node("/root/LevelManager").nextLevel()
+	mCollectibleList.erase(_collectible)
+	if mCollectibleList.size() == 0:
+		get_node("/root/LevelManager").nextLevel()
