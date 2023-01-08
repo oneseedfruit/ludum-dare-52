6b815773e224f7a6cb190158087fee530f01786e
Batbro051939 <isaacleepountney@gmail.com>
2023-01-08 16:55:12 +0800

    fix some level stuff

--- collectible/collectible.gd
+++ collectible/collectible.gd
@@ -3,34 +3,34 @@ extends Area2D
 
 
 var mTileTypeToFrame = {
-    "collectible": 0,
-    "trap": 4,
+	"collectible": 0,
+	"trap": 4,
 }
 
 var mIsTrap : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    connect("body_entered", _on_body_entered)
-    pass # Replace with function body.
+	connect("body_entered", _on_body_entered)
+	pass # Replace with function body.
 
 func _on_body_entered(body):
-    if body.get_script() == Player:
-        if !mIsTrap:
-            # TODO: add points
-            body.stop_at(position)
-            get_parent().collect(self)
-            queue_free()
-        else:
-            body.stop_at(position)
-            body.die()
+	if body.get_script() == Player:
+		if !mIsTrap:
+			# TODO: add points
+			body.stop_at(position)
+			get_parent().collect(self)
+			queue_free()
+		else:
+			body.stop_at(position)
+			body.die()
 
 
 func setType(tileId):
-    if tileId.begins_with("collectible"):
-        $AnimatedSprite2D.set_frame(mTileTypeToFrame["collectible"])
-        mIsTrap = false
-    elif tileId.begins_with("trap"):
-        $AnimatedSprite2D.set_frame(mTileTypeToFrame["trap"])
-        mIsTrap = true
-    pass
+	if tileId.begins_with("collectible"):
+		$AnimatedSprite2D.set_frame(mTileTypeToFrame["collectible"])
+		mIsTrap = false
+	elif tileId.begins_with("trap"):
+		$AnimatedSprite2D.set_frame(mTileTypeToFrame["trap"])
+		mIsTrap = true
+	pass
