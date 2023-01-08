2e32febbdb1976fa9b433fc4997e1dab65c1adc4
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2023-01-08 13:40:12 +0800

    Detect walls, bounce against walls

--- managers/LevelManager.gd
+++ managers/LevelManager.gd
@@ -10,61 +10,60 @@ var mCurrentLevelIndex
 var mCurrentLevelNode
 
 func _init():
-	var dir = DirAccess.open("res://levels")
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if file_name.begins_with("level_") and file_name.ends_with(".tscn"):
-				mLevels.append(file_name)
-			file_name = dir.get_next()
-	pass
+    var dir = DirAccess.open("res://levels")
+    if dir:
+        dir.list_dir_begin()
+        var file_name = dir.get_next()
+        while file_name != "":
+            if file_name.begins_with("level_") and file_name.ends_with(".tscn"):
+                mLevels.append(file_name)
+            file_name = dir.get_next()
+    pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	mCurrentLevelIndex = mStartLevel
-	spawnLevel(mCurrentLevelIndex)
-	pass # Replace with function body.
+    mCurrentLevelIndex = mStartLevel
+    spawnLevel(mCurrentLevelIndex)
+    pass # Replace with function body.
 
 
 func spawnLevel(level):
-	if mCurrentLevelNode != null:
-		mCurrentLevelNode.queue_free()
-		await mCurrentLevelNode.tree_exited
+    if mCurrentLevelNode != null:
+        mCurrentLevelNode.queue_free()
+        await mCurrentLevelNode.tree_exited
 
-	var parentNode = get_node("/root/Game")
-	var collectibleManager = get_node("/root/CollectibleManager")
-	var scene = load("levels/%s" % mLevels[level])
+    var parentNode = get_tree().root.get_node("Game")
+    var scene = load("levels/%s" % mLevels[level])
 
-	mCurrentLevelNode = scene.instantiate()
-	parentNode.add_child.call_deferred(mCurrentLevelNode)
+    mCurrentLevelNode = scene.instantiate()
+    parentNode.add_child.call_deferred(mCurrentLevelNode)
 
-	await mCurrentLevelNode.ready
+    await mCurrentLevelNode.ready
 
-	collectibleManager.spawnCollectibles()
-	
-	_spawnPoint()
+    CollectibleManager.spawnCollectibles()
+    
+    _spawnPoint()
 
-	pass
+    pass
 
 
 func nextLevel():
-	mCurrentLevelIndex = mCurrentLevelIndex + 1
-	if mCurrentLevelIndex < mLevels.size():
-		spawnLevel(mCurrentLevelIndex)
+    mCurrentLevelIndex = mCurrentLevelIndex + 1
+    if mCurrentLevelIndex < mLevels.size():
+        spawnLevel(mCurrentLevelIndex)
 
 
 func _spawnPoint():
-	var tileMap = get_node("/root/Game/LevelTemplate/TileMap")
-	if tileMap:
-		var usedCells = tileMap.get_used_cells(mTileMapObjectLayer)
-		for cellCoord in usedCells:
-			var tileData = tileMap.get_cell_tile_data(mTileMapObjectLayer, cellCoord)
-			var tileId = tileData.get_custom_data("id")
-			if tileId == "player":
-				var playerNode = get_node("/root/Game/Player")
-				var targetPos = tileMap.map_to_local(cellCoord)
-				playerNode.mMoving = Vector2.ZERO
-				playerNode.set_position(targetPos)
-				tileMap.erase_cell(mTileMapObjectLayer, cellCoord)
-	pass
+    var tileMap = get_node("/root/Game/LevelTemplate/TileMap")
+    if tileMap:
+        var usedCells = tileMap.get_used_cells(mTileMapObjectLayer)
+        for cellCoord in usedCells:
+            var tileData = tileMap.get_cell_tile_data(mTileMapObjectLayer, cellCoord)
+            var tileId = tileData.get_custom_data("id")
+            if tileId == "player":
+                var playerNode = get_node("/root/Game/Player")
+                var targetPos = tileMap.map_to_local(cellCoord)
+                playerNode.mMoving = Vector2.ZERO
+                playerNode.set_position(targetPos)
+                tileMap.erase_cell(mTileMapObjectLayer, cellCoord)
+    pass
--- player/player.gd
+++ player/player.gd
@@ -2,59 +2,83 @@ class_name Player
 extends RigidBody2D
 
 # Movement variables (temporarily limit to 250 because too huge will not stop at target)
-@export_range(0, 250) var mMoveSpeed : float = 100
-@onready var mRay = $RayCast2D
+@export_range(0, 250) var mMoveSpeed: float = 100
+@onready var mRay: RayCast2D = $RayCast2D
 
-var mTileSize = 16
-var mInputs = {
-	"left": Vector2.LEFT,
-	"right": Vector2.RIGHT,
-	"up": Vector2.UP,
-	"down": Vector2.DOWN,
+var mTileSize: int = 16
+var mInputs: Dictionary = {
+    "left": Vector2.LEFT,
+    "right": Vector2.RIGHT,
+    "up": Vector2.UP,
+    "down": Vector2.DOWN,
 }
 
-var mMoving = Vector2.ZERO
+var mMoving: Vector2 = Vector2.ZERO
 var mTarget
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	_snap_to_tile()
-	_update_raycast()
-
-
-func _physics_process(delta):
-	var collision = move_and_collide(mMoving * delta * mMoveSpeed)
-	if collision:
-		mMoving = Vector2.ZERO
-		_snap_to_tile()
-
-	if mTarget != null and position.distance_squared_to(mTarget) < pow(delta * mMoveSpeed, 2):
-		position = mTarget
-		mTarget = null
-		mMoving = Vector2.ZERO
-		_snap_to_tile()
-
-	if mMoving == Vector2.ZERO:
-		for dir in mInputs:
-			if Input.is_action_pressed(dir):
-				mMoving = mInputs[dir]
-				_update_raycast()
-
-
-# Reset position to tile
-func _snap_to_tile():
-	position -= Vector2.ONE * mTileSize * 0.5
-	position = position.snapped(Vector2.ONE * mTileSize)
-	position += Vector2.ONE * mTileSize * 0.5
-
-
-# Reset raycast
-func _update_raycast():
-	var targetPos = mMoving * mTileSize
-	mRay.target_position = targetPos
-	mRay.force_raycast_update()
 
 
 # Make player stop at target position
-func stopAt(target):
-	mTarget = target
+func stop_at(target) -> void:
+    mTarget = target
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    _snap_to_tile()
+    _update_raycast()
+
+
+func _physics_process(delta: float) -> void:
+    if mMoving == Vector2.ZERO:
+        for dir in mInputs:
+            if Input.is_action_pressed(dir):
+                mMoving = mInputs[dir]
+                _update_raycast()
+
+    if mTarget != null and position.distance_squared_to(mTarget) < pow(delta * mMoveSpeed, 2):
+        position = mTarget
+        mTarget = null
+        mMoving = Vector2.ZERO
+        _snap_to_tile()
+
+    var collision: KinematicCollision2D = move_and_collide(mMoving * delta * mMoveSpeed)
+    if collision:
+        _on_collision(collision)
+
+
+func _on_collision(collision: KinematicCollision2D) -> void:
+    mMoving = Vector2.ZERO
+    _snap_to_tile()
+
+    var collided_tile_map: TileMap = collision.get_collider() as TileMap
+
+    var collided_at: Vector2 = position + mRay.target_position
+    var tile_coords := collided_tile_map.local_to_map(collided_at)
+
+    var layers_with_walls := [0, 1]
+    for layer in layers_with_walls:
+        var source_id := collided_tile_map.get_cell_source_id(layer, tile_coords, false)
+        var source: TileSetAtlasSource = collided_tile_map.tile_set.get_source(source_id)
+        if source:
+            var atlas_coords := collided_tile_map.get_cell_atlas_coords(layer, tile_coords, false)
+            var tile_data := source.get_tile_data(atlas_coords, 0)
+
+            var is_wall = tile_data.get_custom_data("is_wall")
+            if is_wall:
+                var tween = get_tree().create_tween()
+                tween.tween_property($AnimatedSprite2D, "scale", Vector2(1.1, 1.1), 0.1).set_trans(Tween.TRANS_ELASTIC)
+                tween.tween_property($AnimatedSprite2D, "scale", Vector2.ONE, 0.1).from_current().set_trans(Tween.TRANS_BOUNCE)
+
+
+# Reset position to tile
+func _snap_to_tile() -> void:
+    position -= Vector2.ONE * mTileSize * 0.5
+    position = position.snapped(Vector2.ONE * mTileSize)
+    position += Vector2.ONE * mTileSize * 0.5
+
+
+# Reset raycast
+func _update_raycast() -> void:
+    var targetPos = mMoving * mTileSize
+    mRay.target_position = targetPos
+    mRay.force_raycast_update()
